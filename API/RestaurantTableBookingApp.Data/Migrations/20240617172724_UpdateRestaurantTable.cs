159abef2c28cd8a0903ce56d777bd8e7b47be7ff
Ravindu Wickramage <ravinduyasiska@gmail.com>
2024-06-18 07:28:54 +0530

    Migration for the update of Restaurants table

---
+++ API/RestaurantTableBookingApp.Data/Migrations/20240617172724_UpdateRestaurantTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestaurantTableBookingApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateRestaurantTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OpenTime",
+                table: "Restaurants",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "OpenTime",
+                table: "Restaurants");
+        }
+    }
+}
